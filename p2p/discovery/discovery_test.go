f42590d8203f2181b2d14f1a237c18617e35bb06
Max <82761650+MaxMustermann2@users.noreply.github.com>
2023-02-06 19:07:19 +0000

    Revert "fix p2p discovery test issue"
    This reverts commit e49dfd7824c35c99ac92cfaa94ec020d424f02d2.

--- p2p/discovery/discovery_test.go
+++ p2p/discovery/discovery_test.go
@@ -3,11 +3,9 @@ package discovery
 // TODO: test this module
 
 import (
-	"context"
 	"testing"
 
 	"github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
 )
 
 func TestNewDHTDiscovery(t *testing.T) {
@@ -15,13 +13,7 @@ func TestNewDHTDiscovery(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	ctx, cancel := context.WithCancel(context.Background())
-	var idht *dht.IpfsDHT
-	idht, err = dht.New(ctx, host)
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = NewDHTDiscovery(ctx, cancel, host, idht, DHTConfig{})
+	_, err = NewDHTDiscovery(host, DHTConfig{})
 	if err != nil {
 		t.Fatal(err)
 	}
